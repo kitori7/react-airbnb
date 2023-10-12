760f0bb9598472ce4d7ce899c63ff848b28e76b7
kitori <kitori@126.com>
2023-10-12 16:18:28 +0800

    ✨ feat(10.12): 13天5集

--- src/views/entire/cpns/entire-pagination/style.js
+++ src/views/entire/cpns/entire-pagination/style.js
@@ -1,3 +1,33 @@
 import styled from "styled-components";
 
-export const PaginationWrapper = styled.div``;
+export const PaginationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .desc {
+      margin-top: 16px;
+      color: #222;
+    }
+
+    .MuiPaginationItem-page.Mui-selected {
+      background-color: #000;
+      color: #fff;
+    }
+
+    .MuiPaginationItem-icon {
+      font-size: 24px;
+    }
+
+    .MuiPaginationItem-page {
+      margin: 0 8px;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
--- src/views/entire/cpns/entire-rooms/index.jsx
+++ src/views/entire/cpns/entire-rooms/index.jsx
@@ -1,7 +1,25 @@
 import React, { memo } from "react";
 import { RoomsWrapper } from "./style";
+import { useSelector } from "react-redux";
+import RoomItem from "@/components/room-item";
 const EntireRooms = memo(() => {
-  return <RoomsWrapper>EntireRooms</RoomsWrapper>;
+  const { roomList, totalCount } = useSelector((state) => ({
+    roomList: state.entire.roomList,
+    totalCount: state.entire.totalCount,
+  }));
+
+  return (
+    <RoomsWrapper>
+      <h2 className="title">共{totalCount}处住所</h2>
+      <div className="list">
+        {roomList.map((item) => {
+          return (
+            <RoomItem itemData={item} itemWidth="20%" key={item.id}></RoomItem>
+          );
+        })}
+      </div>
+    </RoomsWrapper>
+  );
 });
 
 export default EntireRooms;
